f28f6e90cb5331d575645c196a2ca6999a22fc8e
shuvokr <anik@sdlknvadijfasdjn.local>
2021-11-10 13:50:32 +0600

    - Added alert handler.

--- Asimple_e-market_application/Controllers/ViewController.swift
+++ Asimple_e-market_application/Controllers/ViewController.swift
@@ -15,7 +15,28 @@ class ViewController: UIViewController {
         print("[ Asimple_e-market_application ]")
     }
     @IBAction func placeOrderAction(_ sender: UIButton) {
-        
+        self.showWarnings(title: "Warning!", alertMessage: "No products have been selected for ordering.")
     }
 }
 
+// MARK: - Alert Handler
+extension ViewController {
+    private func showWarnings(title : String, alertMessage : String) {
+        let alert = UIAlertController(title: title, message: alertMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: { action in
+              switch action.style {
+              case .default:
+                print("default")
+              case .cancel:
+                print("cancel")
+
+              case .destructive:
+                print("destructive")
+              @unknown default:
+                print("Unknown default")
+            }}))
+        DispatchQueue.main.async {
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+}
