0e5a37e5a9fcdc787688a4084a2ea2e023836f91
shuvokr <anik@sdlknvadijfasdjn.local>
2021-11-10 15:14:52 +0600

    - Added store & product model.

--- Asimple_e-market_application/Model/Model.swift
+++ Asimple_e-market_application/Model/Model.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct StoreInfo {
+    var name : String = String()
+    var rating : String = String()
+    var openingTime : String = String()
+    var closingTime : String = String()
+}
+
+struct Products {
+    var name : String = String()
+    var price : String = String()
+    var imageUrl : String = String()
+}
