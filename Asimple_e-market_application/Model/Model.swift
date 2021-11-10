725b4032f2b6185cdb35368cba8ef182d0dad0b6
shuvokr <anik@sdlknvadijfasdjn.local>
2021-11-10 15:13:52 +0600

    - Added webservice engine & data model.

---
+++ Asimple_e-market_application/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Asimple_e-market_application
+//
+//  Created by Shuvo on 11/10/21.
+//
+
+import Foundation
---
+++ Asimple_e-market_application/WebServices/APIService.swift
@@ -0,0 +1,12 @@
+//
+//  APIService.swift
+//  Asimple_e-market_application
+//
+//  Created by Shuvo on 11/10/21.
+//
+
+import UIKit
+
+class APIService: NSObject {
+
+}
