c7fc5dfaa65ea6c2bed20f6b9f779f593e1a8da4
shuvokr <anik@sdlknvadijfasdjn.local>
2021-11-10 15:15:29 +0600

    - Added links & api call func.

--- Asimple_e-market_application/WebServices/APIService.swift
+++ Asimple_e-market_application/WebServices/APIService.swift
@@ -8,5 +8,26 @@
 import UIKit
 
 class APIService: NSObject {
+    private let storeInfoURL = URL(string: "https://virtserver.swaggerhub.com/m-tul/opn-mobile-challenge-api/1.0.0/storeInfo")!
+    private let productListURL = URL(string: "https://virtserver.swaggerhub.com/m-tul/opn-mobile-challenge-api/1.0.0/products")!
+    private let placeOrderURL = URL(string: "https://virtserver.swaggerhub.com/m-tul/opn-mobile-challenge-api/1.0.0/order")!
+}
 
+// MARK: - Load Store Info
+extension APIService {
+    func apiToGetStoreInfo(completion : @escaping (StoreInfo) -> ()) {
+        
+    }
+}
+// MARK: - Load Products
+extension APIService {
+    func apiToGetProductsInfo(completion : @escaping ([Products]) -> ()) {
+        
+    }
+}
+// MARK: - Place order
+extension APIService {
+    func apiToPlaceOrder(completion : @escaping (Int) -> ()) {
+        
+    }
 }
