e0ebd03bcd20629a82b7f78c5c082c78f30a1aec
shuvokr <anik@sdlknvadijfasdjn.local>
2021-11-10 22:02:08 +0600

    - Added network call for store information.

--- Asimple_e-market_application/WebServices/APIService.swift
+++ Asimple_e-market_application/WebServices/APIService.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class APIService: NSObject {
     private let storeInfoURL = URL(string: "https://virtserver.swaggerhub.com/m-tul/opn-mobile-challenge-api/1.0.0/storeInfo")!
@@ -17,6 +19,37 @@ class APIService: NSObject {
 extension APIService {
     func apiToGetStoreInfo(completion : @escaping (StoreInfo) -> ()) {
         
+        let urlTo = URL(string: "https://virtserver.swaggerhub.com/m-tul/opn-mobile-challenge-api/1.0.0/storeInfo")!
+        AF.request(
+            urlTo,
+            method: .get
+            ).responseJSON {
+            (response) in
+            switch response.result {
+                
+                case .success(let json):
+                    let jsonData = JSON(json as Any)
+                    guard let statusCode = response.response?.statusCode else { return }
+                    if(statusCode == 200) {
+                        print(jsonData)
+                        var storeData = StoreInfo()
+                        storeData.name = "Store Name: \(jsonData["name"].stringValue)"
+                        storeData.rating = "Rating: \(jsonData["rating"].stringValue)"
+                        storeData.closingTime = "Opening Time: \(jsonData["openingTime"].stringValue)"
+                        storeData.openingTime = "Closing Time: \(jsonData["closingTime"].stringValue)"
+                        
+                        completion(storeData)
+                    }
+                    else {
+                        print("Found status code ==> \(statusCode)")
+                        completion(StoreInfo())
+                    }
+
+                case .failure(let error):
+                    print("getStoreInfo request error: \(error)")
+                    completion(StoreInfo())
+            }
+        }
     }
 }
 // MARK: - Load Products
