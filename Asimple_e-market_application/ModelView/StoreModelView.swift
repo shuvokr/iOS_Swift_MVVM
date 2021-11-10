49c40ce73326cad3633e10fa7f72922c3c353747
shuvokr <anik@sdlknvadijfasdjn.local>
2021-11-10 22:03:04 +0600

    - Added store info view model action.

--- Asimple_e-market_application/ModelView/StoreModelView.swift
+++ Asimple_e-market_application/ModelView/StoreModelView.swift
@@ -8,5 +8,25 @@
 import UIKit
 
 class StoreModelView: NSObject {
-
+    private var apiService : APIService!
+    private(set) var storeData : StoreInfo! {
+        didSet {
+            self.bindStoreInfoViewModelToController()
+        }
+    }
+    
+    var bindStoreInfoViewModelToController : (() -> ()) = {}
+    
+    override init() {
+        super.init()
+        print("5")
+        self.apiService =  APIService()
+        callFuncToGetStoreData()
+    }
+    
+    func callFuncToGetStoreData() {
+        self.apiService.apiToGetStoreInfo { (storeData) in
+            print(storeData)
+        }
+    }
 }
