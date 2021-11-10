ecba535c0d967ae3797055085ea9ffaf7962c142
shuvokr <anik@sdlknvadijfasdjn.local>
2021-11-10 13:15:28 +0600

    - Config project architecture.

--- Asimple_e-market_application/Controllers/ViewController.swift
+++ Asimple_e-market_application/Controllers/ViewController.swift
@@ -12,8 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("[ Asimple_e-market_application ]")
     }
-
-
 }
 
